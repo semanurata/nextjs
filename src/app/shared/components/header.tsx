16567bc41d8d2df729214e96c9a934a2d73b336d
Sema Nur Ata <atasemanur@icloud.com>
2025-10-07 21:55:36 +0300

    added header & footer

--- src/app/shared/components/header.tsx
+++ src/app/shared/components/header.tsx
@@ -1,11 +1,40 @@
-"use client";
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import { SunIcon } from "lucide-react";
 
-const Header = () => {
+const Navbar02Page = () => {
   return (
-    <>
-      <div className="text-white py-2 md:py-3 bg-[#0f2f1a]">test</div>
-    </>
+    <div className="min-h-screen bg-muted">
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-(--breakpoint-xl) mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center gap-8">
+            <Logo />
+
+            {/* Desktop Menu */}
+            <NavMenu className="hidden md:block" />
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Button variant="outline" className="hidden sm:inline-flex">
+              Sign In
+            </Button>
+            <Button>Sign Up</Button>
+            <Button size="icon" variant="outline">
+              <SunIcon />
+            </Button>
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
-export { Header };
+export default Navbar02Page;
+
