5c1138d976a14cce87526cba1b2de523f716052a
Sema Nur Ata <atasemanur@icloud.com>
2025-10-13 00:03:55 +0300

    added nav items

--- src/app/sahiplendirme/page.tsx
+++ src/app/sahiplendirme/page.tsx
@@ -1,7 +1,17 @@
+import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "@/components/ui/terminal"
 export default function Sahiplendirme() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p>test</p>
+      <Terminal>
+  <TypingAnimation>pnpm dlx shadcn@latest init</TypingAnimation>
+  <AnimatedSpan>✔ Preflight checks.</AnimatedSpan>
+  <AnimatedSpan>✔ Validating Tailwind CSS.</AnimatedSpan>
+  <TypingAnimation>Success! Project initialization completed.</TypingAnimation>
+</Terminal>
     </div>
   );
 }
